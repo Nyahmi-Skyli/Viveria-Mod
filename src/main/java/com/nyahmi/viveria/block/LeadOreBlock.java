1a60b6aeceab0c4189c7cd047d5e52da687039a0
Nyahmi-Skyli <66649421+Nyahmi-Skyli@users.noreply.github.com>
2021-09-22 07:58:28 -0400

    Added some basic stuffs

---
+++ src/main/java/com/nyahmi/viveria/block/LeadOreBlock.java
@@ -0,0 +1,65 @@
+
+package com.nyahmi.viveria.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.util.ForgeSoundType;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import com.nyahmi.viveria.ViveriaModElements;
+
+@ViveriaModElements.ModElement.Tag
+public class LeadOreBlock extends ViveriaModElements.ModElement {
+	@ObjectHolder("viveria:lead_ore")
+	public static final Block block = null;
+	public LeadOreBlock(ViveriaModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK)
+					.sound(new ForgeSoundType(1.0f, 1.0f, () -> new SoundEvent(new ResourceLocation("block.netherite_block.break")),
+							() -> new SoundEvent(new ResourceLocation("block.netherite_block.step")),
+							() -> new SoundEvent(new ResourceLocation("block.netherite_block.place")),
+							() -> new SoundEvent(new ResourceLocation("block.netherite_block.hit")),
+							() -> new SoundEvent(new ResourceLocation("block.netherite_block.fall"))))
+					.hardnessAndResistance(1.2f, 8f).setLightLevel(s -> 0).harvestLevel(1).harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("lead_ore");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
