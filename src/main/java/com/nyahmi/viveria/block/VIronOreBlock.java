ae0bb84b8d22650c65617e4e4422de86e5879462
Nyahmi-Skyli <66649421+Nyahmi-Skyli@users.noreply.github.com>
2021-09-26 06:41:41 -0400

    added iron ore and brown stones

--- src/main/java/com/nyahmi/viveria/block/VIronOreBlock.java
+++ src/main/java/com/nyahmi/viveria/block/VIronOreBlock.java
@@ -2,19 +2,17 @@
 package com.nyahmi.viveria.block;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.common.util.ForgeSoundType;
 import net.minecraftforge.common.ToolType;
 
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
@@ -24,11 +22,11 @@ import java.util.Collections;
 import com.nyahmi.viveria.ViveriaModElements;
 
 @ViveriaModElements.ModElement.Tag
-public class LeadOreBlock extends ViveriaModElements.ModElement {
-	@ObjectHolder("viveria:lead_ore")
+public class VIronOreBlock extends ViveriaModElements.ModElement {
+	@ObjectHolder("viveria:v_iron_ore")
 	public static final Block block = null;
-	public LeadOreBlock(ViveriaModElements instance) {
-		super(instance, 1);
+	public VIronOreBlock(ViveriaModElements instance) {
+		super(instance, 4);
 	}
 
 	@Override
@@ -39,14 +37,9 @@ public class LeadOreBlock extends ViveriaModElements.ModElement {
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK)
-					.sound(new ForgeSoundType(1.0f, 1.0f, () -> new SoundEvent(new ResourceLocation("viveria:metal_new")),
-							() -> new SoundEvent(new ResourceLocation("block.netherite_block.step")),
-							() -> new SoundEvent(new ResourceLocation("viveria:metal_new")),
-							() -> new SoundEvent(new ResourceLocation("viveria:metal_new")),
-							() -> new SoundEvent(new ResourceLocation("block.netherite_block.fall"))))
-					.hardnessAndResistance(1.2f, 8f).setLightLevel(s -> 0).harvestLevel(1).harvestTool(ToolType.PICKAXE).setRequiresTool());
-			setRegistryName("lead_ore");
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.NETHERITE).hardnessAndResistance(1.4f, 8f).setLightLevel(s -> 0)
+					.harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("v_iron_ore");
 		}
 
 		@Override
