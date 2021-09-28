a4f34a6e480402978720b372d9b014b3da231265
Nyahmi-Skyli <66649421+Nyahmi-Skyli@users.noreply.github.com>
2021-09-28 00:15:35 -0400

    new stuffs

---
+++ src/main/java/com/nyahmi/viveria/block/MeteoriteBlock.java
@@ -0,0 +1,93 @@
+
+package com.nyahmi.viveria.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+import com.nyahmi.viveria.particle.EmberRedParticle;
+import com.nyahmi.viveria.ViveriaModElements;
+
+@ViveriaModElements.ModElement.Tag
+public class MeteoriteBlock extends ViveriaModElements.ModElement {
+	@ObjectHolder("viveria:meteorite")
+	public static final Block block = null;
+	public MeteoriteBlock(ViveriaModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.NETHERITE).hardnessAndResistance(1.9999999999999998f, 800f)
+					.setLightLevel(s -> 6).harvestLevel(3).harvestTool(ToolType.PICKAXE).setRequiresTool()
+					.setNeedsPostProcessing((bs, br, bp) -> true).setEmmisiveRendering((bs, br, bp) -> true));
+			setRegistryName("meteorite");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.GOLD;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public void animateTick(BlockState blockstate, World world, BlockPos pos, Random random) {
+			super.animateTick(blockstate, world, pos, random);
+			PlayerEntity entity = Minecraft.getInstance().player;
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (true)
+				for (int l = 0; l < 4; ++l) {
+					double d0 = (x + random.nextFloat());
+					double d1 = (y + random.nextFloat());
+					double d2 = (z + random.nextFloat());
+					int i1 = random.nextInt(2) * 2 - 1;
+					double d3 = (random.nextFloat() - 0.5D) * 0.25D;
+					double d4 = (random.nextFloat() - 0.5D) * 0.25D;
+					double d5 = (random.nextFloat() - 0.5D) * 0.25D;
+					world.addParticle(EmberRedParticle.particle, d0, d1, d2, d3, d4, d5);
+				}
+		}
+	}
+}
